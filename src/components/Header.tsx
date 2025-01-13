5a2b2f927b459a980f605aa0b94dd9c1a4b8d728
Chaithanya <103263681+Chaithanya-Matta@users.noreply.github.com>
2025-01-12 22:30:14 -0600

    adding login web page

---
+++ src/components/Header.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+// import {Link} from 'react-router-dom'
+
+const Header = () => {
+  return (
+    <>
+        <nav className='navbar container'>
+            {/* <Link className='navbar-brand text-light' to='/'>Trello Clone</Link> */}
+            <a className='navbar-brand text-light' href="/">Trello Clone</a>
+
+            <button className='btn btn-info'>Sign up</button>
+
+        </nav>
+    </>
+  )
+}
+
+export default Header
---
+++ src/components/Login.tsx
@@ -0,0 +1,33 @@
+import React from 'react'
+
+const Login = () => {
+  return (
+    <>
+        <div className='container'>
+            <div className='row justify-content-center'>
+                <div className='col-md-6 bg-light-dark p-5 rounded'>
+                    <h3 className='text-light'>Login to Trello Clone</h3>
+
+                    <form >
+
+                        <div className='mb-3'>
+                            <input type="text" className='form-control' placeholder='Username' />
+                        </div>
+                        <div className='mb-3'>
+                            <input type="passsword" className='form-control' placeholder='Password' />
+                        </div>
+
+                        <button type='submit' className='btn btn-info d-block mx-auto'>Login</button>
+
+                    </form>
+
+                </div>
+
+            </div>
+
+        </div>
+    </>
+  )
+}
+
+export default Login
